7f13a30f46da27850ac2913782e89f3be9e73e05
COLTREX369 <j.colorado@utp.edu.co>
2024-03-21 20:14:13 -0500

    actualizando

---
+++ Frontend/src/App.js
@@ -0,0 +1,39 @@
+
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
+import Footer from './Components/Footer/Footer';
+import ropa_banner from './Components/Assets/banner_ropa.png'
+import electrodomesticos_banner from './Components/Assets/banner_electrodomesticos.png'
+import gamer_banner from './Components/Assets/banner_gamer.png'
+import joyas_banner from './Components/Assets/banner_joyas.png'
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/ropa' element={<ShopCategory banner={ropa_banner} category="ropa"/>}/>
+        <Route path='/electrodomesticos' element={<ShopCategory banner={electrodomesticos_banner} category="electrodomesticos"/>}/>
+        <Route path='/gamer' element={<ShopCategory banner={gamer_banner} category="gamer"/>}/>
+        <Route path='/joyeria' element={<ShopCategory banner={joyas_banner} category="joyeria"/>}/>
+        <Route path="/product" element={<Product/>}>
+          <Route path=":productId" element={<Product/>}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
