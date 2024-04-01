16690dc089d212217d818d50adcf858f49926ba8
Molchu <soymolchu@gmail.com>
2024-03-31 19:18:21 -0500

    corrections of database table

--- Backend/databasepg.js
+++ Backend/databasepg.js
@@ -24,18 +24,14 @@ app.get('/', (req, res) => {
 app.use(bodyParser.json());
 
 app.post('/login', async (req, res) => {
-    const { nombre, apellido, correo, cedula, ciudad, fecha_nacimiento, contrasena } = req.body;
+    const { nombre, apellido, correo, telefono, fecha_nacimiento, contrasena } = req.body;
     try {
         const existingUser = await client.query('SELECT * FROM usuario WHERE correo=$1', [correo]);
-        const existingId = await client.query('SELECT * FROM usuario WHERE cedula=$1', [cedula]);
-        if (existingId.rows.length>0) {
-            return res.status(400).json({ error: 'documento de identidad ya registrado' })
-        }
         if (existingUser.rows.length>0) {
             return res.status(400).json({ error: 'correo ya registrado' })
         }
 
-        await client.query('INSERT INTO usuario (nombre, apellido, correo, cedula, ciudad, fecha_nacimiento,contrasena) VALUES ($1, $2, $3, $4, $5, $6, $7)', [nombre, apellido, correo, cedula, ciudad, fecha_nacimiento,contrasena]);
+        await client.query('INSERT INTO usuario (nombre, apellido, correo, telefono, fecha_nacimiento, contrasena) VALUES ($1, $2, $3, $4, $5, $6)', [nombre, apellido, correo, telefono, fecha_nacimiento,contrasena]);
         res.status(201).json({ message: 'Usuario creado exitosamente' });
     } catch (error) {
         console.error('Error al crear usuario:', error);
