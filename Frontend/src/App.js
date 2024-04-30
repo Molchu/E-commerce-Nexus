ee8d185d3a28faf233f762ded4de4af58f7225d4
anderx0510 <anderson.arciniegas@utp.edu.co>
2024-04-30 18:15:39 -0500

    search interface

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -18,33 +18,32 @@ import Topbar from './Components/Topbar/Topbar'
 import ShowSearch from './Pages/ShowSearch';
 import GiftCard from './Pages/GiftCard';
 
-
 function App() {
   const [results, setResults] = useState([]);
+  const [showSearchResults, setShowSearchResults] = useState(false);
+
   return (
     <div>
       <BrowserRouter>
-      <Topbar setResults={setResults}/>
-      <Navbar/>
-      <ShowSearch results={results} />
-      <Routes>
-        <Route path='/' element={<Shop/>}/>
-        <Route path='/Ropa' element={<ShopCategory banner={ropa_banner} category="Ropa"/>}/>
-        <Route path='/Electrodomesticos' element={<ShopCategory banner={electrodomesticos_banner} category="Electrodomesticos"/>}/>
-        <Route path='/Gamer' element={<ShopCategory banner={gamer_banner}  category="Gamer"/>}/>
-        <Route path='/Joyeria' element={<ShopCategory banner={joyas_banner} category="Joyeria"/>}/>
-        <Route path="/product" element={<Product/>}>
-        <Route path=":productId" element={<Product/>}/>
-
-        </Route>
-        <Route path='/showsearch' element={<ShowSearch/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/signup' element={<Signup/>}/>
-        <Route path='/signin' element={<Signin/>}/>
-        <Route path='/perfil' element={<Profile/>}/>
-        <Route path='/regalo' element={<GiftCard/>}/>
-      </Routes>
-      <Footer/>
+        <Topbar setResults={setResults} setShowSearchResults={setShowSearchResults} />
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop />} />
+          <Route path='/search-results' element={<ShowSearch results={results} />} />
+          <Route path='/Ropa' element={<ShopCategory banner={ropa_banner} category="Ropa" />} />
+          <Route path='/Electrodomesticos' element={<ShopCategory banner={electrodomesticos_banner} category="Electrodomesticos" />} />
+          <Route path='/Gamer' element={<ShopCategory banner={gamer_banner}  category="Gamer" />} />
+          <Route path='/Joyeria' element={<ShopCategory banner={joyas_banner} category="Joyeria" />} />
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/perfil' element={<Profile />} />
+          <Route path='/regalo' element={<GiftCard />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
